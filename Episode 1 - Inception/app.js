918d5ecb0e6f6780f0aa1e2214b46d728ee4c7a0
Love Poonia <chlovepoonia26@gmail.com>
2025-07-22 13:30:00 +0530

    Episode 1 - Inception

---
+++ Episode 1 - Inception/app.js
@@ -0,0 +1,12 @@
+var heading = React.createElement("div", {id: "parent"}, 
+    [React.createElement("div", {id: "child"}, 
+        [React.createElement("h1", {id: "hello"}, "Hello Love" ), 
+            React.createElement("h1", {id: "hello2"}, "hii" )]),
+            React.createElement("div", {id: "child2"} , 
+                [React.createElement("h1", {id: "hello2"}, "Hello Love" ),
+                     React.createElement("h1", {id: "hello2"}, "hii" )])]);
+        
+var root = ReactDOM.createRoot(document.getElementById("root"));
+
+        
+root.render(heading);
